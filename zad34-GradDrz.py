a1d09dcbdad8639b22ca46bfc785938b71c3e4ae
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-05-03 21:13:20 +0200

    Zad 34 i 35 za zadaću

---
+++ zad34-GradDrz.py
@@ -0,0 +1,4 @@
+# Grad-država igra (u listu ili dictionary ubaciti parove gl.grad - država i random izvlačiti za igru)
+# u listu ili dictionary ubaciti niz parova država-grad koje ćemo koristiti u igri
+# random izvlačiti "računalno" odabrani grad ili zemlju, pa tražiti od korisnika da unese drugi dio
+#brojati pokušaje u obliku X/Y ili "samo" točno / netočno
---
+++ zad35-UmjKliz.py
@@ -0,0 +1,2 @@
+# Umjetničko klizanje (izračun bodova i rang liste) google pravila -> min, max
+# ocjene od 0.0 do 6.0, najveća i najmanja se odbacuju, do 9 sudaca, minimalno 4, unosi korisnik
