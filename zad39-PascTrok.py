a51d6fdcd8f4bd1c89c498a3dd3f05fbc6c67c85
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-05-10 22:53:47 +0200

    Pascal pokusaj 1 i 2

--- zad39-PascTrok.py
+++ zad39-PascTrok.py
@@ -1,7 +1,74 @@
-#zad39 - Pascalov trokut
+"""#zad39 - Pascalov trokut
 #Unijeti broj redova pascalovog trokuta (do max 10)
 #formatirano ispisati koristeći tabulator
 #                  1
 #              1       1
 #          1       2       1
 #      1       3       3       1
+
+*****************************NE RADI**************************************
+
+brojRedova = int(input("Unesite broj redova za Pascalov trokut: "))
+
+previousRow = []
+currentRow = []
+counter = 0
+counterTab = brojRedova
+
+def formulaZaIspis():
+    global previousRow
+    global currentRow
+    global counter
+    global counterTab
+
+    #brojac
+    counter = 0
+    #
+    currentRow = previousRow
+
+    
+    currentRow.append(1)
+    print("\t"*counterTab, end = "")
+    for int in currentRow:
+        
+        counter = 0
+        if  counter < len(currentRow)/2:
+            if len(previousRow) > 2 and counter != currentRow[0]:
+                print(previousRow[int]+previousRow[int+1], end = "\t\t")
+                counter += 1
+            elif int == currentRow[0]:
+                print(int, end = "\t\t")
+                counter += 1
+            elif int == currentRow[-1]:
+                print(int, end = "\t\t")
+                counter += 1
+            elif len(previousRow) > 2:
+                print(previousRow[int]+previousRow[int+1], end = "\t\t")
+                counter += 1
+        elif counter >= len(currentRow)/2:
+            if len(previousRow) < 2:
+                print(previousRow[int]+previousRow[int+1], end = "\t\t")
+                counter -= 1
+            elif int == currentRow[0]:
+                print(int, end = "\t\t")
+                counter -= 1
+            elif int == currentRow[-1]:
+                print(int, end = "\t\t")
+                counter -= 1
+            elif len(previousRow) < 2:
+                print(previousRow[int]+previousRow[int+1], end = "\t\t")
+                counter -= 1
+        else:
+            print("????")
+        print(f"c={counter}!", end ="")
+            
+    
+    #print(f"podjela {len(currentRow)/2}",len(currentRow),end = "")
+    print()
+    counterTab -= 1
+    previousRow = currentRow
+
+for broj in range(brojRedova):
+    formulaZaIspis()
+
+"""
---
+++ zad39-pokusaj2.py
@@ -0,0 +1,36 @@
+#zad39 - Pascalov trokut
+#Unijeti broj redova pascalovog trokuta (do max 10)
+#formatirano ispisati koristeći tabulator
+#                  1
+#              1       1
+#          1       2       1
+#      1       3       3       1
+
+# formula https://hr.wikipedia.org/wiki/Binomni_koeficijent
+
+def binomniKoeficijent(broj,koef):
+    rezultat = 1
+
+    for x in range(koef):
+        rezultat = rezultat * (broj-x)
+        rezultat = rezultat // (x+1)
+    return rezultat
+
+test1 = binomniKoeficijent(4,2)
+
+
+brojRedova = 0
+while brojRedova < 1 or brojRedova > 10:
+    brojRedova = int(input("Unesite broj redova za Pascalov trokut (do 10): "))
+    if brojRedova < 1 or brojRedova > 10:
+        print("Broj nije u navedenom rasponu, pokušajte ponovno!")
+
+# brojač za formatiranje redova
+counterTab = brojRedova
+
+for red in range(brojRedova):
+    print("\t"*counterTab, end = "")
+    for brojevi in range(red+1):
+        print(binomniKoeficijent(red,brojevi), end ="\t\t")
+    counterTab -= 1
+    print()
