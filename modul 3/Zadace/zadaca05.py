8ac6d8547d408b10341166823314db5abba71393
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-06-14 21:09:49 +0200

    zadaca05

---
+++ modul 3/Zadace/zadaca05.py
@@ -0,0 +1,4 @@
+#proširiti program koji preuzima prognozu sa neta "json_meteo1.py" + "meteo.json"
+#potencijalno imati bazu gradova
+#isto tako probati neki meni ukomponirati
+#pokušati koristiti requests umjesto urllib
