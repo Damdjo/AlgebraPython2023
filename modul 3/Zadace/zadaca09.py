64e765c82cff4ed0f260560d07c5a3252083aba7
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-06-30 21:07:21 +0200

    zadaca09 Baze podataka

---
+++ modul 3/Zadace/zadaca09.py
@@ -0,0 +1,6 @@
+# Napraviti bazu korisnika u kojoj će administrator(!mi!) moći dodavati/skidati korisnike
+# grupirati ih po odjelima(nrp. HR,IT, prodaja)
+# i upravljati sa njihovim pravima/dozvolama
+
+#svaka grupa/dozvola ima različiti meni 
+#meni ne mora imati funkciju 
