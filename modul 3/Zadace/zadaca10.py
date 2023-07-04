c37c37a5c644166aff5e4379f19ec2b10f1530ce
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-07-04 21:07:51 +0200

    Predavan je 04.07.2023 GUI tkinter + zadaca10

--- modul 3/Zadace/zadaca10.py
+++ modul 3/Zadace/zadaca10.py
@@ -4,3 +4,6 @@
 #2 labela u kojima će se prikazivati kombinacije 1.(5x50) 2.(2x12)
 #centrirano piše Eurojackpot
 #na dnu je gumb u kojem piše eurojackpot ili daj kombinaciju
+
+
+#loto zadaća je \Sve prije modula 3\zad33-LotoGen.py
