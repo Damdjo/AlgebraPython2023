6cda0b22e34543d9ab325481083ac400258e2c06
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-07-06 21:12:53 +0200

    Predavanje 6.7 tkinter + zadaca11 + tk_keys vodic

---
+++ modul 3/Zadace/zadaca11.py
@@ -0,0 +1,7 @@
+#napraviti sustav za logiranje korisnik koji će sadržavati sliku, pravima itd.
+
+#ukomponirati: bazu podataka, slike koje se nalaze na računalu
+
+#kada se korisnik logira, prikazuju se njegovi podatci, slika 
+
+#gumb:, change, save, itd
---
+++ modul 3/tkinter/tk9.py
@@ -0,0 +1,22 @@
+"""def inkremetiraj(broj):
+    return broj+1
+
+a = 5
+
+a = inkremetiraj(a)
+print(a)
+
+"""
+
+inkrementiraj = lambda a : a+1
+def zbroji(x,y):
+    pass
+
+print(inkrementiraj(5))
+x = 5
+y = 3
+
+#button_zbroji = tk.Button(root, text="Zbroji", command=lambda: zbroji(x,y))
+
+#način na koji u tkinteru pozvati funkciju sa argumentima
+#command=lambda: funkcija(argument¸1,argument2)
