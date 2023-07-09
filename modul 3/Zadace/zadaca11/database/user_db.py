ec387ed81501a44b9e07a54b2bcabc36ec70c306
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-07-09 17:11:26 +0200

    Goto add users dio

--- modul 3/Zadace/zadaca11/database/user_db.py
+++ modul 3/Zadace/zadaca11/database/user_db.py
@@ -38,7 +38,7 @@ def db_execute (query_to_execute:str, db_path):
 
 
 #Funkcija SELECT koja isto tako i ispisuje vrijednosti koje dohvati
-def ispis_racuna (type:int, column:str="", value="", db_path:str="user_accounts.db") -> tuple:
+def ispis_racuna (type:int, column:str="", value="", db_path:str="user_accounts.db") -> tuple | str | list:
     """
     Funkcija ima 3 tipa rada\n
     Tip 1: unosi se samo broj 1 kao prvi argument te funkcija ispisuje cijelu tablicu users\n
@@ -47,10 +47,10 @@ def ispis_racuna (type:int, column:str="", value="", db_path:str="user_accounts.
         2. argumentstupac prema kojem pretražujemo
         3. argument vrijednost u tom stupcu
     """
-
+    select_query = "SELECT * FROM users"
     match type:
         case 1:
-            select_query = "SELECT * FROM users"
+            pass
         case 2:
             select_query = f"""
             SELECT * FROM users
@@ -67,7 +67,7 @@ def ispis_racuna (type:int, column:str="", value="", db_path:str="user_accounts.
         cursor = connection.cursor()
         match type:
             case 1:
-                cursor.execute(select_query)
+                cursor.execute("SELECT * FROM users")
                 data_cur = cursor.fetchall()
             case 2:
                 cursor.execute(select_query)
@@ -76,12 +76,17 @@ def ispis_racuna (type:int, column:str="", value="", db_path:str="user_accounts.
 
         
     finally:
-        if connection and data_cur:            
+        if connection and data_cur and type == 1:            
+            connection.close()
+            return data_cur
+        elif connection and data_cur and type == 2:
             connection.close()
             return data_cur[0]
+        else:
+            return "User does not exist"
     return ()
         
-def check_login(usr_uname, usr_pword, db_path) -> list | str:
+def check_login(usr_uname, usr_pword, db_path) -> list | str | tuple:
     """
     funkcija koja koristeći 2 tip funkcije "ispis_racuna" provjerava username i password koje je korisnik unjeo te ako postoji takva kombinacija vraća
     listu [username, odjel, level]
@@ -108,162 +113,6 @@ def check_login(usr_uname, usr_pword, db_path) -> list | str:
             else:#slucaj username kriv
                 return "Username/password\n incorrect!", False
             
-
-##TODO - delete
-def iface(user_data:list):
-    """Funckija za ispis sučelja"""
-    #popis opcija menija po odjelima
-    odjel_admin = ["Dodaj novog usera", "Uredi postojećeg usera", "Izbriši usera", "Ispis popisa usera"]
-    odjel_nabava = ["PLACEHOLDER_NABAVA_1", "PLACEHOLDER_NABAVA_2", "PLACEHOLDER_NABAVA_3", "PLACEHOLDER_NABAVA_4"]
-    odjel_HR = ["PLACEHOLDER_HR_1", "PLACEHOLDER_HR_2", "PLACEHOLDER_HR_3", "PLACEHOLDER_HR_4"]
-
-    
-
-    
-    print("*"*60)
-    print(f"User: {str(user_data[0]).capitalize()}".center(25)," "*17, f"Odjel: {user_data[1]}".center(18))
-    print("\n")
-    match str(user_data[1]).upper():
-        case "IT":
-            options_list = odjel_admin
-        case "NABAVA":
-            options_list = odjel_nabava
-        case "HR":
-            options_list = odjel_HR
-    print("  MENI:")
-    for broj, opcija in enumerate(options_list):
-        print(f"\t{broj+1}. {str(opcija).capitalize()}")
-    print(f"\n\t0. Izlaz")
-
-##TODO
-def choices(user_data:list):
-    status = -1    
-    while status != 0:
-        os.system("cls")        
-        iface(user_data)
-        try:
-            status = int(input("\nUnesite odabir: "))
-
-        except ValueError as e:
-            print(f"Unos mora biti broj!")
-            continue
-        else:
-            odjel = user_data[1]
-        
-        match odjel:
-            case "IT": # Za sada je napravljen samo ovaj meni, ostali su placeholderi
-                match status:
-                    case 0:
-                        print("Izlaz iz programa")
-                        break
-                    case 1: # Slucaj gdje se dodaje novi korisnik, traži se unos svih podataka te ima double check da je sve dobro uneseno
-                        os.system("cls")
-                        print("Unos novog usera\n")
-                        username = input("\tUnesite username: ")
-                        password = input("\tUnesite password: ")
-                        odjel = input("\tUnesite odjel: ")
-                        level = input("\tUnesite level: ")
-                        to_insert = (f"{username}",f"{password}",f"{odjel}",f"{level}")
-                        print(f"Uneseni podatci su:\nUser: {username} | Pass: {password} | Odjel: {odjel} | Level: {level}")
-                        input_check = input("\nJeste li sigurni da želite potvrditi navedeno? (DA/NE)\n\n\tOdabir: ")
-                        match input_check.upper():
-                            case "DA":
-                                db_execute(to_insert)
-                            case _:
-                                pass
-                        
-                    case 2:
-                        os.system("cls")
-                        print("Uređivanje postojećeg usera\n")
-
-                        try:
-                            edit_user = int(input("Unesite id usera kojeg želite urediti: "))
-                        except ValueError as val:
-                            print(f"Krivi unos: {val}")
-                            continue
-
-
-                        exists_check = ispis_racuna(3,"userID",edit_user) #3. Tip funkcije vraća () ako zapis ne postoji 
-                        if exists_check == ():                            # to možemo ovdje iskoristiti da se ne ispiše tablica stupaca bez ikakvog sadržaja
-                            print("Navedeni user ne postoji!")
-                            continue
-                        unchanged = ispis_racuna(2,"userID",edit_user)[1:] #spremamo original, slice tako da ne uzimamo userID
-                        print("Polja koja ne želite mijenjati ostavit prazna!")
-                        is_changed = True #defaultno postavljamo kao True te se kod svakog unosa provjerava da nije prazan
-                        username = input("\tUnesite username: ")
-                        if username =="":
-                            username = unchanged[0]
-                        else:
-                            is_changed = False # 
-                        password = input("\tUnesite password: ")
-                        if password =="":
-                            password = unchanged[1]
-                        else:
-                            is_changed = False
-                        odjel = input("\tUnesite odjel: ")
-                        if odjel =="":
-                            odjel = unchanged[2]
-                        else:
-                            is_changed = False
-                        level = input("\tUnesite level: ")
-                        if level =="":
-                            level = unchanged[3]
-                        else:
-                            is_changed = False
-
-                        if is_changed: # ako su svi unosi prazni tj. ništa novo nije uneseno onda se izvršava sljedeće
-                            print("Podatci nisu izmjenjeni (Ništa novo nije uneseno)!")
-                            continue
-
-                        #ukoliko je neki od unosa promjenjen onda se nastavlja sa izvršavanjem
-                        new = (f"{username}",f"{password}",f"{odjel}",f"{level}")
-                        update_query = f"""
-                        UPDATE users 
-                        SET username = '{username}', password = '{password}', department = '{odjel}', level = {level}
-                        WHERE userID = {edit_user}
-                        """
-
-                        #ispis sa starim i novim podatcima
-                        os.system("cls")
-                        print(f"{''.center(6)} | {'Username'.center(12)} | {'Password'.center(25)} | {'Department'.center(10)} | {'Level'.center(4)}")
-                        
-                        print(f"STARO  | {str(unchanged[0])[:12].center(12)} | {str(unchanged[1])[:25].center(25)} | {str(unchanged[2])[:10].center(10)} | {str(unchanged[3])[:4].center(4)}")
-                        
-                        print(f" NOVO  | {str(new[0])[:12].center(12)} | {str(new[1])[:25].center(25)} | {str(new[2])[:10].center(10)} | {str(new[3])[:4].center(4)}")
-                        double_check = input("\nJeste li sigurni da želite potvrditi navedeno? (DA/NE)\n\n\tOdabir: ")
-                        match double_check.upper():
-                            case "DA":                                
-                                db_execute(update_query)
-                            case _:
-                                pass
-                        
-
-                    case 3:                        
-                        os.system("cls")
-                        print("Brisanje usera\n")
-                        
-                        del_user = int(input("Unesite id usera kojeg želite izbrisati: "))
-                        delete_query = f"DELETE FROM users WHERE userID = '{del_user}'"
-                        exists_check = ispis_racuna(3,"userID",del_user) #3. Tip funkcije vraća () ako zapis ne postoji 
-                        if exists_check == ():                           # to možemo ovdje iskoristiti da se ne ispiše tablica stupaca bez ikakvog sadržaja
-                            print("Navedeni user ne postoji!")
-                            continue
-                        os.system("cls")
-                        #Budući da je kod nastavio, možemo ispisati tablicu koja će sadržavati usera 
-                        ispis_racuna(2,"userID",del_user)
-                        double_check = input("\nJeste li sigurni da želite izbrisati usera? (DA/NE)\n\n\tOdabir: ")
-                        match double_check.upper():
-                            case "DA":                                
-                                db_execute(delete_query)
-                            case _:
-                                pass
-                    case 4:
-                        os.system("cls")
-                        print("Popis usera\n")
-                        ispis_racuna(1)
-                    
-                    #case 5:
-                        #db_execute("SELECT * FROM accounts")
 
 
 #END funkcije
@@ -311,11 +160,18 @@ def create_table(db_path:str="user_accounts.db"):
 def main():
     
 #Hardcodiranje admin usera na prvom pokretanju
-    #to_insert = ("admin","admin123","IT","5")
-    #db_insert(to_insert)
+
     os.chdir("modul 3/Zadace/zadaca11/database")
-    usr_data = check_login()
-    choices(usr_data)
+    """db_path = "user_accounts.db"
+    create_table(db_path)
+    admin_hardcode = ("admin","admin123","IT","5")
+    test = make_insert_query(admin_hardcode)    
+    db_execute(test, db_path) """
+
+    
+    data = ispis_racuna(1)
+    for row in data:
+        print(row)
         
         
 if __name__ == "__main__":
--- modul 3/Zadace/zadaca11/main.py
+++ modul 3/Zadace/zadaca11/main.py
@@ -16,6 +16,8 @@ ctk.set_default_color_theme("green")
 db_path = "database/user_accounts.db"
 logged_in = False
 admin_seg_btn_menu = ""
+department_list = ["IT","Sales","HR","Exec"]
+level_list = ["1", "2", "3", "4", "5"]
 #funkcije
 
 #Ulogiranje u program
@@ -23,14 +25,17 @@ def sign_in(event):
     global logged_in
     uname = app.sidebar_uname_input.get()
     pword = app.sidebar_pword_input.get()
-    message, logged_in = db.check_login(uname,pword,db_path)
-    app.event_log_text.set(message)
+    credentials, logged_in = db.check_login(uname,pword,db_path)
+    app.event_log_text.set(credentials)
+    
+    print(credentials)# lista: [username, odjel, level]
+
     # ovo zamijeni sidebar sa drugim ako je login uspješan
     if logged_in:
         app.sidebar_login_frame.grid_remove()
         app.sidebar_user_frame.grid()
         app.center_frame.grid()
-    
+    print(db.ispis_racuna(1,db_path=db_path))
 
     print()
 #Odlogiranje iz programa
@@ -59,38 +64,114 @@ def sign_out(event):
 def test(event):
     ctk.set_appearance_mode("light")
     
-def test_seg(event):
-    text = app.center_seg_button_text.get()
-    print(text)
 
-def seg_button_change(value):
-    global admin_seg_btn_menu
-    print(admin_seg_btn_menu)
-    admin_seg_btn_menu = value
+def show_all_users():
+    #data = db.ispis_racuna(1,db_path=db_path)
+    data = db.ispis_racuna(1,db_path=db_path)
+    print(data)
+    #print(data)
+###funkcije za admin segmentirani meni
 
-    match admin_seg_btn_menu:
+#mijenja između menija tako da ostale gasi
+def change_seg_menu(selected_option):    
+    app.focus() 
+    match selected_option:
         case "Show user list":
-            app.center_admin_show_user.grid() ####Stisnuti
+            app.center_admin_show_user.grid() ####Stisnuti gumb
             app.center_admin_add_user.grid_remove()
             app.center_admin_modify_user.grid_remove()
             app.center_admin_delete_user.grid_remove()
+            show_all_users()
         case "Add user":
+            clear_add_user()
             app.center_admin_show_user.grid_remove()
-            app.center_admin_add_user.grid() ####Stisnuti
+            app.center_admin_add_user.grid() ####Stisnuti gumb
             app.center_admin_modify_user.grid_remove()
             app.center_admin_delete_user.grid_remove()
         case "Modify user":
             app.center_admin_show_user.grid_remove()
             app.center_admin_add_user.grid_remove()
-            app.center_admin_modify_user.grid() ####Stisnuti
+            app.center_admin_modify_user.grid() ####Stisnuti gumb
             app.center_admin_delete_user.grid_remove()
         case "Delete user":
             app.center_admin_show_user.grid_remove()
             app.center_admin_add_user.grid_remove()
             app.center_admin_modify_user.grid_remove()
-            app.center_admin_delete_user.grid() ####Stisnuti
+            app.center_admin_delete_user.grid() ####Stisnuti gumb
         case _:
             pass
+
+#dept drop down meni 
+def get_add_user_dept_dropdown(selected_dept):
+    return selected_dept
+
+#level drop down meni 
+def get_add_user_level_dropdown(selected_level):
+    return selected_level
+
+#dodaje unesene podatke u bazu
+def add_user_submit():
+    check = True
+    message = ""
+    uname = app.add_uname.get()
+    if uname == "":
+        message +="Username cannot be empty!"
+        check = False
+    pword = app.add_pword.get()
+    if pword == "":
+        message += "*Password cannot be empty!"
+        check = False
+    dept = app.add_dept.get()
+    if dept not in department_list:
+        message += "*Department must be selected!"
+        check = False
+    level = app.add_level.get()
+    if level not in level_list:
+        message += "*Level must be selected!"
+        check = False    
+
+    if check:
+        message = db.ispis_racuna(2,"username",uname,db_path=db_path)
+        match message:
+            case "User does not exist":
+                data_tuple = (uname, pword, dept, level)
+                ins_query = db.make_insert_query(data_tuple)
+                print(ins_query)
+                db.db_execute(ins_query, db_path)                
+                
+            case _:
+                app.add_error_log.configure(text = "Username is already taken!")
+    else:
+        message = message.replace("*","\n")
+        app.add_error_log.configure(text = message)
+
+                
+    
+    
+    #ovo sam bindao na login button i samo jednom pokrenuo da se kreira baza sa hardkodiranim admin userom
+    """ 
+    db.create_table(db_path)
+    admin_hardcode = ("admin","admin123","IT","5")
+    test = db.make_insert_query(admin_hardcode)    
+    db.db_execute(test, db_path)
+    """
+#briše sve uneseno u add user meni-ju
+def clear_add_user():
+    app.focus() 
+    app.add_uname.delete(0,ctk.END)
+    app.add_uname.configure(placeholder_text = "Username:")
+    app.add_pword.delete(0,ctk.END)
+    app.add_pword.configure(placeholder_text = "Password:")
+    app.add_dept.set("Department")
+    app.add_level.set("Level")
+#funkcija koja se izvrši kada se klikne jedan od gumbiju
+def seg_button_change(value):
+    global admin_seg_btn_menu
+    admin_seg_btn_menu = value
+    print(admin_seg_btn_menu)
+    change_seg_menu(admin_seg_btn_menu)
+
+    
 
 
 
@@ -114,9 +195,9 @@ class App(ctk.CTk):
         self.geometry(f"{1100}x{500}")
 
         # definiranje grida
-        self.grid_columnconfigure((1,2,3), weight= 0)
+        self.grid_columnconfigure((1,2,3), weight= 0) # type: ignore
         self.grid_rowconfigure(0, weight=0)
-        self.grid_rowconfigure((1,2,3,4), weight= 1)
+        self.grid_rowconfigure((1,2,3,4), weight= 1) # type: ignore
         
     #####sidebar 1 gdje je login + profil info
         self.sidebar_login_frame = ctk.CTkFrame(self, width=175, corner_radius=0)
@@ -166,7 +247,7 @@ class App(ctk.CTk):
         self.center_frame = ctk.CTkFrame(self, width=600, corner_radius=5, fg_color="transparent")
         self.center_frame.grid(row = 0, column = 1, rowspan = 5, columnspan = 2, padx = 20, pady = 5, sticky = "ns")
         self.center_frame.grid_rowconfigure(0, weight = 0)
-        self.center_frame.grid_rowconfigure((1,2,3,4), weight = 1)
+        self.center_frame.grid_rowconfigure((1,2,3,4), weight = 1) # type: ignore
         #Center title
         self.center_title_text = ctk.StringVar()
         self.center_title_text.set("PLACEHOLDER")
@@ -175,43 +256,78 @@ class App(ctk.CTk):
       ###Center frame #POTENCIJALNO SAMO ZA ADMINA, NOVI ZA USERE  
         self.center_switch_frame = ctk.CTkFrame(self.center_frame, width=600, corner_radius=5, fg_color="transparent")
         self.center_switch_frame.grid(row = 1, column = 1, rowspan = 4, columnspan = 2, padx = 20, pady = 5, sticky = "ns")
-        self.center_switch_frame.grid_rowconfigure(1, weight=1)
+        self.center_switch_frame.grid_rowconfigure(0, weight=0)
+        self.center_switch_frame.grid_rowconfigure((1,2,3,4), weight=1) # type: ignore
         ##################
         #Center tab button
         ##################
         self.center_seg_button_text = ctk.StringVar(value = "")
-        self.center_seg_button = ctk.CTkSegmentedButton(self.center_switch_frame, command=seg_button_change, variable=self.center_seg_button_text)
-        self.center_seg_button.grid(row = 0, column = 1, padx = 5, pady = (0, 5), columnspan = 2, sticky = "nsew")
+        self.center_seg_button = ctk.CTkSegmentedButton(self.center_switch_frame,width=600, command=seg_button_change, variable=self.center_seg_button_text)
+        self.center_seg_button.grid(row = 0, column = 0, columnspan = 2, padx = 5, pady = (0, 5), sticky = "nsew")
         self.center_seg_button.configure(values = ["Show user list","Add user","Modify user","Delete user"])
         #Center tab
         ###Show user tab 
         self.center_admin_show_user = ctk.CTkFrame(self.center_switch_frame, width=600, corner_radius=5)
         self.center_admin_show_user.grid(row = 1, column = 0, rowspan = 4, columnspan = 2, padx = 20, pady = 5, sticky = "nsew")
-        self.admin_show_user = ctk.CTkLabel(self.center_admin_show_user, text= "Show user list")
-        self.admin_show_user.pack()
-        self.center_admin_show_user.grid_remove()
+        self.admin_show_user = ctk.CTkLabel(self.center_admin_show_user, text= "Show user list".center(20), width=560)
+        self.admin_show_user.grid(row = 1, column = 0, padx = 20, pady = 5)
+        
+
+        #self.center_admin_show_user.grid_remove()
+
         ###Add tab  
         self.center_admin_add_user = ctk.CTkFrame(self.center_switch_frame, width=600, corner_radius=5)
         self.center_admin_add_user.grid(row = 1, column = 0, rowspan = 4, columnspan = 2, padx = 20, pady = 5, sticky = "nsew")
-        self.admin_add_user = ctk.CTkLabel(self.center_admin_add_user, text= "Add user")
-        self.admin_add_user.pack()
+        self.center_admin_add_user.grid_columnconfigure((0,1,2,3,4), weight=0) # type: ignore
+        self.admin_add_user = ctk.CTkLabel(self.center_admin_add_user, text= "Add user".center(20), width=560)
+        self.admin_add_user.grid(row = 0, column = 0,columnspan = 5, padx = 20, pady = (5,50))
+
+        self.add_userid = ctk.CTkLabel(self.center_admin_add_user, width = 40, text="User ID: ")
+        self.add_userid.grid(row = 1, column = 0, padx = (15,5), pady = 5)
+        self.add_uname = ctk.CTkEntry(self.center_admin_add_user, width = 140, placeholder_text="Username: ")
+        self.add_uname.grid(row = 1, column = 1, padx = 5, pady = 5)
+        self.add_pword = ctk.CTkEntry(self.center_admin_add_user, width = 140, placeholder_text="Password: ")
+        self.add_pword.grid(row = 1, column = 2, padx = 5, pady = 5)
+        
+        self.add_dept = ctk.CTkOptionMenu(self.center_admin_add_user, width = 120, values=department_list, command=get_add_user_dept_dropdown)
+        self.add_dept.grid(row = 1, column = 3, padx = 5, pady = 5)
+
+        self.add_level = ctk.CTkOptionMenu(self.center_admin_add_user, width = 75, values = level_list)
+        self.add_level.grid(row = 1, column = 4, padx = (5,15), pady = 5)
+        #confirm button
+        self.add_confirm = ctk.CTkButton(self.center_admin_add_user, width = 75, text = "Confirm", command=add_user_submit)
+        self.add_confirm.grid(row = 2, column = 1, columnspan = 2, padx = 5, pady = 25)
+        #clear button
+        self.add_confirm = ctk.CTkButton(self.center_admin_add_user, width = 75, text = "Clear input",
+                                          command=clear_add_user, fg_color= ("gray15", "gray34"), hover_color= ("gray17","gray24"))
+        self.add_confirm.grid(row = 2, column = 2, columnspan = 2, padx = 5, pady = 25)
+        self.add_error_log = ctk.CTkLabel(self.center_admin_add_user, width = 480, text = "")
+        self.add_error_log.grid(row = 3, column = 0, columnspan = 5, padx = 5, pady = 5)
+
+        #remove menu grid
         self.center_admin_add_user.grid_remove()
+
         ###Modify user tab  
         self.center_admin_modify_user = ctk.CTkFrame(self.center_switch_frame, width=600, corner_radius=5)
         self.center_admin_modify_user.grid(row = 1, column = 0, rowspan = 4, columnspan = 2, padx = 20, pady = 5, sticky = "nsew")
-        self.admin_modify_user = ctk.CTkLabel(self.center_admin_modify_user, text= "Modify user")
-        self.admin_modify_user.pack()
+        self.admin_modify_user = ctk.CTkLabel(self.center_admin_modify_user, text= "Modify user".center(20), width=560)
+        self.admin_modify_user.grid(row = 1, column = 0, padx = 20, pady = 5)
+
+
         self.center_admin_modify_user.grid_remove()
+
         ###Delete user tab  
         self.center_admin_delete_user = ctk.CTkFrame(self.center_switch_frame, width=600, corner_radius=5)
         self.center_admin_delete_user.grid(row = 1, column = 0, rowspan = 4, columnspan = 2, padx = 20, pady = 5, sticky = "nsew")
-        self.admin_delete_user = ctk.CTkLabel(self.center_admin_delete_user, text= "Delete user")
-        self.admin_delete_user.pack()
+        self.admin_delete_user = ctk.CTkLabel(self.center_admin_delete_user, text= "Delete user".center(20), width=560)
+        self.admin_delete_user.grid(row = 1, column = 0, padx = 20, pady = 5)
+
+
         self.center_admin_delete_user.grid_remove()
 
 
         #Sakrivanje menija dok se korisnik ne ulogira
-        self.center_frame.grid_remove()
+        #self.center_frame.grid_remove()
         
         
 
@@ -228,19 +344,7 @@ class App(ctk.CTk):
 
 
 
-"""
-self.center_tabview = ctk.CTkTabview(self.center_frame, width=800)
-        self.center_tabview.grid(row = 1, column = 1, padx = 5, pady = (0, 5), sticky = "nsew")
-        #tabs
-        self.center_tabview.add("Show user list")
-        self.center_tabview.tab("Show user list").grid_columnconfigure(0, weight=1)
-        self.center_tabview.add("Add user")
-        self.center_tabview.tab("Add user").grid_columnconfigure(1, weight=1)
-        self.center_tabview.add("Modify user")
-        self.center_tabview.tab("Modify user").grid_columnconfigure(2, weight=1)
-        self.center_tabview.add("Delete user")
-        self.center_tabview.tab("Delete user").grid_columnconfigure(3, weight=1)
-"""
+
 
 
 
