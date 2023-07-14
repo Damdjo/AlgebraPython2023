9e00ca20ee785947103c5f36888ee1c5e4271699
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-07-14 21:13:46 +0200

    Početak modul 4

---
+++ modul 4/pametna_kuca.py
@@ -0,0 +1,26 @@
+""" 
+ANTE LINKOVI
+    https://www.youtube.com/@ExplainingComputers/videos
+    https://www.youtube.com/@Dronebotworkshop/videos
+    https://www.youtube.com/@JeffGeerling/videos
+    https://projecthub.arduino.cc/trending
+    https://hackaday.com/blog/ 
+    https://community.openhab.org/t/hacking-the-lidl-silvercrest-zigbee-gateway-a-step-by-step-tutorial/129660
+    https://www.hackster.io/hartmut-wendt/raspberry-pi-based-weather-station-a9a7dd       
+   
+"""
+
+
+
+###generalno
+
+#pametne utičnice sa mjerenjem potrošnje i remote paljenjem/gašenjem
+#rasvjeta sa upravljenjem putem aplikacije ali i fizički putem prekidača + potencijalno senzori pokreta koji će paliti svjetla, osim usred noći
+#senzori temperature i vlage po stanu čije informacije će se koristiti za paljenje/gašenje klime
+
+###dnevni
+
+#zavjese na motorima koje će po ljeti zamračiti prostor preko dana
+#mjerenje vlažnosti tla u vazama od biljaka te slanje poruke kada treba zaliti
+
+
