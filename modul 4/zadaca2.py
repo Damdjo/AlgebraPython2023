1e6ae013ec9715d875435eaec98b598fdc93dd56
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-07-20 21:13:48 +0200

    Zadaca predavanje 20.7

---
+++ modul 4/zadaca2.py
@@ -0,0 +1,5 @@
+#OPCIJA 1: Zmijica
+#OPCIJA 2: Memory s kockicama 2x2 (ukupno 16 polja, 8 boja za pamćenje)
+#OPCIJA 3: space invaders (mali avion u dnu displaya, L-r, kockice padaju odozgo)
+#OPCIJA 4: breaker ( plocice sirine 2x3, balun 1px, plocice u nebu u boji, 1-2 px)
+#OPCIJA 5: po volji
