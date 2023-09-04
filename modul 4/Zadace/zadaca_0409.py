702d062c43a75f8c8e64c1131775cdfe3a141119
Damdjo <23003363+Damdjo@users.noreply.github.com>
2023-09-04 21:07:59 +0200

    Rename

---
+++ modul 4/Zadace/zadaca_0409.py
@@ -0,0 +1,10 @@
+#ako unesemo unos za NOVI uzorak:
+""" a) uzorak je različit od postojećih, spremamo ga
+    b) uzorak već postoji, kroz konzolu ili grafički porukom ponuditi:
+        L -> izbrisi
+        D -> izmjeni uzorak
+      klikom na srednji gumb sprema se
+
+
+
+"""
